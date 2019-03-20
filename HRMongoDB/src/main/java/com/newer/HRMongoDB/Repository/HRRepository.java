b7125dddb806c7d25bf2cc56c726554f09571845
llj <1653887048@qq.com>
2019-03-20 18:33:10 +0800

    Revert "添加原始后端"
    This reverts commit 617a4a4a339eddec708658013572079473e51488.

--- HRMongoDB/src/main/java/com/newer/HRMongoDB/Repository/HRRepository.java
+++
@@ -1,9 +0,0 @@
-package com.newer.HRMongoDB.Repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.newer.HRMongoDB.HR;
-
-public interface HRRepository extends MongoRepository<HR, String> {
-
-}
